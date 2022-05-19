3a9244c7265a8048762bae4576853cd156fd645f
Daven Young <davenyoung@msn.com>
2022-05-19 12:34:30 -0600

    commit tree 0d13c158e6df6f7ed102b3de17e8708d352fbde4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
-
+class Book
+    attr_accessor :title :author :page_count :genre
+    
+    def initialize(title ="And Then There Were None", author, page_count, genre)
+        @title=title
+        @author=author
+        @page_count=page_count
+        @genre=genre
+    end
+end
 
